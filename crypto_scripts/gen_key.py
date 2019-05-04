b61a9538323d3fc937cec6f09504b7800e4fa582
Liamlhy <liamlilhy@gmail.com>
2019-05-04 14:54:00 +0200

    sign python and new gen_key

--- crypto_scripts/gen_key.py
+++ crypto_scripts/gen_key.py
@@ -2,18 +2,28 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.PublicKey import RSA
 
-def gen_keypair(): 
-    secret_code = "Unguessable"
-    key = RSA.generate(2048)
-    encrypted_key = key.export_key(passphrase=secret_code, pkcs=8,
-                              protection="scryptAndAES128-CBC")
 
-    file_out = open("rsa_privkey.bin", "wb")
-    file_out.write(encrypted_key)
+def generate_RSA(bits=2048):
+    '''
+    Generate an RSA keypair with an exponent of 65537 in PEM format
+    param: bits The key length in bits
+    Return private key and public key
+    '''
+    from Crypto.PublicKey import RSA
+    new_key = RSA.generate(bits, e=65537)
+    public_key = new_key.publickey().exportKey("PEM")
+    private_key = new_key.exportKey("PEM")
+
+    file_out = open("rsa_privkey.pem", "wb")
+    file_out.write(private_key)
     file_out.close()
 
-    file_out = open("rsa_pubkey.bin", "wb")
-    file_out.write(key.publickey().export_key())
+    file_out = open("rsa_pubkey.pem", "wb")
+    file_out.write(public_key)
     file_out.close()
+    return private_key, public_key
 
-gen_keypair()
+
+
+
+generate_RSA()
---
+++ crypto_scripts/sign.py
@@ -0,0 +1,30 @@
+import sys, getopt
+from base64 import b64encode
+from Crypto.Signature import PKCS1_PSS
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+
+
+message = "I want this stream signed"
+temp = message.encode(encoding='utf_8')
+h = SHA256.new()
+h.update(temp)
+
+
+private_key = False
+with open ("rsa_privkey.pem", "r") as myfile:
+    private_key = RSA.importKey(myfile.read())
+
+public_key = False
+with open ("rsa_pubkey.pem", "r") as myfile:
+    public_key = RSA.importKey(myfile.read())
+
+signer = PKCS1_v1_5.new(private_key)
+sig = signer.sign(h)
+
+
+verifier = PKCS1_v1_5.new(public_key)
+verified = verifier.verify(h, sig)
+assert verified, 'Signature verification failed'
+print ('Successfully verified message')
