6c6317cf70ed173cc35606188e6f336a8e0bdd9b
Liamlhy <liamlilhy@gmail.com>
2019-05-11 17:57:39 +0200

    gen_key changed, passphrase C was wrong

--- crypto_scripts/gen_key.py
+++ crypto_scripts/gen_key.py
@@ -2,6 +2,11 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.PublicKey import RSA
 
+# Generate the RSA key for every user
+
+def gen_key_pair(passpharse):
+    new_key = RSA.generate(bits, e=65537)
+    return key.exportKey(passphrase=passphrase), key.publickey().exportKey()
 
 def generate_RSA(bits=2048):
     '''
@@ -10,30 +15,58 @@ def generate_RSA(bits=2048):
     Return private key and public key
     '''
     from Crypto.PublicKey import RSA
+
+
     new_key = RSA.generate(bits, e=65537)
     public_key = new_key.publickey().exportKey("PEM")
-    private_key = new_key.exportKey("PEM")
+    private_key = new_key.exportKey(passphrase='this_is_C')
 
-    file_out = open("rsa_privkey.pem", "wb")
+    file_out = open("rsa_privkey_C.pem", "wb")
     file_out.write(private_key)
     file_out.close()
 
-    file_out = open("rsa_pubkey.pem", "wb")
+    file_out = open("rsa_pubkey_C.pem", "wb")
     file_out.write(public_key)
     file_out.close()
     return private_key, public_key
 
+    # new_key = RSA.generate(bits, e=65537)
+    # public_key = new_key.publickey().exportKey("PEM")
+    # private_key = new_key.exportKey("PEM")
+    #
+    # file_out = open("rsa_privkey_B.pem", "wb")
+    # file_out.write(private_key)
+    # file_out.close()
+    #
+    # file_out = open("rsa_pubkey_B.pem", "wb")
+    # file_out.write(public_key)
+    # file_out.close()
+    # return private_key, public_key
+    #
+    # new_key = RSA.generate(bits, e=65537)
+    # public_key = new_key.publickey().exportKey("PEM")
+    # private_key = new_key.exportKey("PEM")
+    #
+    # file_out = open("rsa_privkey_C.pem", "wb")
+    # file_out.write(private_key)
+    # file_out.close()
+    #
+    # file_out = open("rsa_pubkey_C.pem", "wb")
+    # file_out.write(public_key)
+    # file_out.close()
+    # return private_key, public_key
 
 
 
-<<<<<<< HEAD
+
+# <<<<<<< HEAD
 generate_RSA()
-=======
-def gen_shared_key():
-    file_out = open("shared_privkey.bin","wb")
-    file_out.write( get_random_bytes(16))
-    file_out.close()
+#
+# def gen_shared_key():
+#     file_out = open("shared_privkey.bin","wb")
+#     file_out.write( get_random_bytes(16))
+#     file_out.close()
 
 
-gen_shared_key()
->>>>>>> fa48ca8975a9acb18c4f95ce16d9373accce6e03
+
+# >>>>>>> fa48ca8975a9acb18c4f95ce16d9373accce6e03
